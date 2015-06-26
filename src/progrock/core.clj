73c352d1622b79a7f832a5f206212ea636a4cd78
James Reeves <jreeves@weavejester.com>
2015-06-26 16:06:21 +0200

    Add options for complete and incomplete characters

--- src/progrock/core.clj
+++ src/progrock/core.clj
@@ -16,14 +16,16 @@
 (defn- keyword-replace [string keywords]
   (reduce-kv #(str/replace %1 (str %2) (str %3)) string keywords))
 
-(defn- bar-text [{:keys [progress total]} {:keys [length]}]
-  (let [completed (int (* (/ progress total) length))]
-    (str (apply str (repeat completed "="))
-         (apply str (repeat (- length completed) " ")))))
+(defn- bar-text [{:keys [progress total]} {:keys [length complete incomplete]}]
+  (let [completed-length (int (* (/ progress total) length))]
+    (str (apply str (repeat completed-length complete))
+         (apply str (repeat (- length completed-length) incomplete)))))
 
 (def default-options
   {:length 50
-   :format "[:bar] :progress/:total"})
+   :format "[:bar] :progress/:total"
+   :complete \=
+   :incomplete \space})
 
 (defn as-string
   ([bar]
