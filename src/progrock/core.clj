1fd08ff1482ed98cd78418376fe5a3b143e84afc
James Reeves <jreeves@weavejester.com>
2015-06-26 12:58:25 +0200

    Remove closure, split progress-bar data from display

--- src/progrock/core.clj
+++ src/progrock/core.clj
@@ -1,11 +1,25 @@
 (ns progrock.core)
 
-(defn progress-bar
-  [{:keys [total length]}]
-  (fn [progress]
-    (let [completed       (/ progress total)
-          complete-length (int (* completed length))]
-      (str "["
-           (apply str (repeat complete-length "="))
-           (apply str (repeat (- length complete-length) " "))
-           "]"))))
+(defn progress-bar [total]
+  {:progress 0, :total total})
+
+(defn tick
+  ([bar]
+   (tick bar 1))
+  ([bar amount]
+   (update-in bar [:progress] + amount)))
+
+(defn done [bar]
+  (assoc bar :progress (:total bar)))
+
+(defn as-string
+  ([bar]
+   (as-string bar {}))
+  ([bar options]
+   (let [completed       (/ (:progress bar) (:total bar))
+         bar-length      (:length options 50)
+         complete-length (int (* completed bar-length))]
+     (str "["
+          (apply str (repeat complete-length "="))
+          (apply str (repeat (- bar-length complete-length) " "))
+          "]"))))
