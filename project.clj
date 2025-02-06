039da1d960e55b9f91bb52ed71db8f12f15ddbc7
James Reeves <jreeves@weavejester.com>
2025-02-06 12:30:58 +0000

    Update minimum Clojure version to 1.9

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/weavejester/progrock"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :plugins [[codox "0.8.13"]])
